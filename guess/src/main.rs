2589e18efecfdef5735fa280197fd18a5e89b42f
lawzheng <602285510@qq.com>
2024-01-21 11:16:59 +0800

    猜字游戏完结

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -5,19 +5,29 @@ use std::io;
 fn main() {
     println!("猜数游戏");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("秘密数字是：{}", secret_number);
-    println!("猜测一个数");
-    
-    let mut guess = String::new();
+    // println!("秘密数字是：{}", secret_number);
 
-    io::stdin().read_line(&mut guess).expect("无法读取行");
+    loop {
+        println!("猜测一个数");
 
-    let guess: u32 = guess.trim().parse().expect("please type a number");
-    println!("你猜测的数字是：{}", guess);
+        let mut guess = String::new();
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("small"),
-        Ordering::Greater => println!("big"),
-        Ordering::Equal => println!("you win"),
-    };
+        io::stdin().read_line(&mut guess).expect("无法读取行");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("你猜测的数字是：{}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("small"),
+            Ordering::Greater => println!("big"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            },
+        };
+    }
 }
